6b2875f71724204e57769cf5f5d0a610e4aced2f
MaxenceGlt <maxencegirault69@gmail.com>
2023-12-03 16:00:53 +0100

    mise en place du theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
+import { ThemeContext } from '../index.js'
 
 function Navbar() {
+    const [state, setState] = useContext(ThemeContext);
+
+    function toggleTheme(){
+        setState(state => ({
+            ...state,
+            theme: state.theme === 'black' ? 'light' : 'black'
+          }))
+    }
     return (
-        <div>
-            
+        <div className={`${state.themes[state.theme].backGroundColorRadiantRecap}`}>
+            <div className={`ml-aut o`}>
+                <button
+                className={`h-10 w-10 bg-peach opacity-1 rounded-lg ml-aut o ${state.themes[state.theme].bg}`}
+                onClick={()=>toggleTheme()}
+                >
+                    {state.theme === "light" ? (
+                        <div>
+                            <p>test</p>
+                        <SunIcon color={"white"} boxSize={16} />
+                        </div>
+                        )
+                        
+                    :(
+                        <div>
+                        <p>tetatast</p>
+                        <MoonIcon color={"black"} boxSize={16} />
+                        </div>
+                    )}
+                </button>
+                <button
+                className=" h-10 w-10 bg-myPurple opacity-1 rounded-lg"
+                onClick={() => setState(state => ({
+                    ...state,
+                    theme: state.theme === 'light' ? 'black' : 'light'
+                  }))}
+                >
+                </button>
+            </div>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, createContext } from "react";
 import './index.css';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Routes, Route, Outlet} from "react-router-dom";
@@ -6,16 +6,36 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 
+const ThemeContext = createContext();
 
 function Layout(){
+
+  const [state, setState] = useState({
+    theme: 'light',
+    themes: {
+      light: {
+        backGroundColorRadiantRecap: "bg-purple-600",
+        bg: "bg-red-200",
+      },
+      black: {
+        //Commun
+        backGroundColorRadiantRecap: "bg-purple-200",
+        bg: "bg-red-800",
+      },
+    },
+  });
+
   return (
     <div  className="min-h-full w-full">
-        <Navbar />
-        <Outlet />
-        <Footer className="" />
+        <ThemeContext.Provider value={[state, setState]}>
+          <Navbar />
+          <Outlet />
+          <Footer className="" />
+        </ThemeContext.Provider>
     </div>
   );
 }
+
 ReactDOM.render(
   <React.StrictMode>
     <div className="Container mx-auto">
@@ -33,3 +53,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+export { ThemeContext };
